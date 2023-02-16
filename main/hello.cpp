dfd1694cc4dd56424c7f9a058656c5e4468022a2
vidishachandra <77980305+vidishachandra@users.noreply.github.com>
2023-02-16 09:23:25 +0530

    First commit

---
+++ main/hello.cpp
@@ -0,0 +1,10 @@
+//File: hello.cpp
+#include <iostream>
+using namespace std;
+
+int main(void) {
+  cout << "Hello, World PES1UG20CS498\n";
+  cout << "Hello, Jenkins\n";
+  cout << "I have successfully built and run\n";
+  return 0;
+}
